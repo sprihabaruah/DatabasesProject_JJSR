ce80757cf22008db9643e3200c374ce052593ff2
joonamkim <jnk8843@gmail.com>
2015-12-06 18:21:53 -0500

    hi spriha

--- 550/routes/index.js
+++ 550/routes/index.js
@@ -28,12 +28,16 @@ module.exports = function(app, passport) {
     // show the login form
     app.get('/login', function(req, res) {
 
+    	
+    	
         // render the page and pass in any flash data if it exists
         res.render('loginPage.ejs', { message: req.flash('loginMessage') }); 
     });
 
     // process the login form
     app.post('/login', passport.authenticate('local-login', {
+    	
+    	
         successRedirect : '/profile', // redirect to the secure profile section
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
@@ -266,6 +270,8 @@ function titanicGame (req, res) {
         unusedQuestionNumbers = [0,1]; //TODO make this NOT hardcoded. Should be equal to the number of questions each game has
         newGame = true;
 
+        
+        
         res.render('score.ejs', {finalScore: finalScore});
         return;
     }
